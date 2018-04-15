c9ebf81beef861ff8f40cd301e9834c4c299fd46
Philippe Recto <precto1285@gmal.com>
2018-04-15 13:56:57 -0700

    added MySQL file

---
+++ bamazon_products.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE bamazon;
+
+item_idCREATE TABLE products
+(
+	item_id INT,
+    product_name VARCHAR(50),
+    department_name VARCHAR(50),
+    price INT,
+    stock_quantity INT
+    
+);
