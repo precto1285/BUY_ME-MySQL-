215315b2d43f56e4666c4dfbf6ad6daacdbe9281
Philippe Recto <precto1285@gmal.com>
2018-04-15 17:46:40 -0700

    bamazonCustomer reading sql file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,13 +1,12 @@
 var Inquirer = require('inquirer');
 var fs = require('fs');
 
-fs.readFile("./bamazon_products.sql", 'utf8') function(err,data){
+fs.readFile("./bamazon_products.sql", 'utf8', function(err, data){
     if(err){
         return console.log(err);
     }
-
     console.log(data);
 
-};
+});
 
 
