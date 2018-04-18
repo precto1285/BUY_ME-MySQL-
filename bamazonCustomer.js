c82235a531fdc8182b33cb364c1307de2a40c04e
Philippe Recto <precto1285@gmal.com>
2018-04-18 14:46:47 -0700

    restart js fiile.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,19 +1,15 @@
-const MySql = require('mysql');
+const Mysql = require('mysql');
 
-const con = MySql.createConnection({
-    host:'bamazon',
-    user:'root',
-    password:'P!n0YR0ck*',
-    database:'bamazon'
+const con = Mysql.createConnection({
+    host:'127.0.0.1',
+    port:3306,
+    user:"root",
+    password:"P!n0yR0ck*",
+    database:"bamazon"
 });
 
 con.connect(function(err){
-    if (err) return err;
-    console.log("Connected!");
-});
-
-const fs = require('fs');
-
-fs.readFile(con, function(){
-    console.log(con);
+    if (err) throw err;
+    console.log("Connected as ID ") + con.threadId;
+    connection.end();
 });
