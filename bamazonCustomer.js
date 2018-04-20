ff377f9cac734e9300275edaa1c250dfc2b316d7
Philippe Recto <precto1285@gmal.com>
2018-04-19 18:23:26 -0700

    fixed sql connecton

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,33 +1,29 @@
-const Mysql = require('mysql');
+const mysql = require('mysql');
 
-const fs = require('fs');
-
-const mySqlbc = fs.readFile('./bamazon_products.js');
-
-const con = Mysql.createConnection({
+const con = mysql.createConnection({
     file: mySqlbc,
-    host:'localhost',
+    host:'127.0.0.1',
     port:3306,
     user:"root",
     password:"P!n0yR0ck*",
     database:"bamazon"
 });
 
-connection.connect(function(err){
+con.connect(function(err){
     if (err) throw err;
-    console.log("Connected as ID ") + connection.threadId;
+    console.log("Connected as ID ") + con.threadId;
     readProducts();
-    connection.end();
+    con.end();
 });
 
 function readProducts() {
     console.log("Selecting all products...\n");
-    connection.quer(
+    con.query(
         "SELECT * FROM products", function (err, res) {
             if (err) throw err;
 
             console.log(res);
-            connection.end();
+            con.end();
         }
     );
 }
